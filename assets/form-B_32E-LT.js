9f048e9d8b2cba596bee4c66ad6072969f15a345
mrkorzun <60797864+mrkorzun@users.noreply.github.com>
2026-04-22 11:08:10 +0000

    Deploying to gh-pages from @ mrkorzun/goit-js-hw-09@9ea22e69532815ab29d180113c15a0af2d343042 🚀

--- assets/form-B_32E-LT.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),btn:document.querySelector(".btn")};console.log(e);function n({target:o}){console.log(o)}e.form.addEventListener("input",n);
-//# sourceMappingURL=form-B_32E-LT.js.map
