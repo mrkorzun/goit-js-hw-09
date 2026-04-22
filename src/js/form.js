9ea22e69532815ab29d180113c15a0af2d343042
Romario Korzun <60797864+mrkorzun@users.noreply.github.com>
2026-04-22 15:07:44 +0400

    прописал все три шага по ТЗ к форме (запись, чтение, удаление)

--- src/js/form.js
+++ src/js/form.js
@@ -1,16 +1,67 @@
 const refs = {
   form: document.querySelector('.feedback-form'),
-  btn: document.querySelector('.btn'),
 };
-console.log(refs);
+// console.log(refs);
 
-const formData = {
+let formData = {
   email: '',
   message: '',
 };
 
+// вызываем сразу шаг 2, перед шагом 1
+fieldForm();
+
+// шаг 1: чекаем данные, сохраняем в обьект и выносим в LS
 function onFormFieldChange({ target: formFieldEl }) {
-  console.log(formFieldEl);
+  // куда пишем
+  const formFieldName = formFieldEl.name;
+  // что пишем
+  const formFieldValue = formFieldEl.value.trim();
+  // сохраняем в обьект
+  formData[formFieldName] = formFieldValue;
+  // сохраняеем обьект в LS (задаем название и переводим обьект в строчку)
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+// шаг 2: если есть данные в LS то выводим в input
+function fieldForm() {
+  // считать данные LS
+  try {
+    const formDataLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+    // проверить есть ли данные в LS 
+    if(formDataLS === null) {
+      return;
+    }
+    // делаю этот обьект не пустым если даныне уже были записаны до LS
+    formData = formDataLS;
+    // выносим данные в input c помощью перебора Object.keys()
+    const formDataLsKeys = Object.keys(formDataLS);
+    // записываем значение по ключу, которое берем из refs.form.lements в input
+    formDataLsKeys.forEach(key => {
+      refs.form.elements[key].value = formDataLS[key];
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  
+};
+
+// шаг 3: проверка на то что все поля заполнены и чистка формы и LS после submit
+function onFormSubmit(event) {
+  event.preventDefault();
+  // собираем масыв всего что юзер ввёл
+  const formDataValues = Object.values(formData);
+  // проверка на пустые строчки
+  const emptyFieldName = Object.keys(formData).find(key => !formData[key].trim());
+  if(formDataValues.includes('')) {
+    console.log(`Пусте полe для введення - ${emptyFieldName}`);
+    return;
+  }
+  // чистим LS 
+  localStorage.removeItem('feedback-form-state');
+  // чистим форму
+  refs.form.reset();
 }
 
 refs.form.addEventListener('input', onFormFieldChange);
+refs.form.addEventListener('submit', onFormSubmit);
